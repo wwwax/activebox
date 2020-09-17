092e076f5d85bb48d896f412ca21c52f9d51fe6d
Max Ivanov <waxivanov@gmail.com>
2020-09-17 15:22:51 +0300

    upd

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
 let isMenuOpen = false;
 
 burger.addEventListener('click', handlerBurgerClick);
@@ -8,9 +9,11 @@ burger.addEventListener('click', handlerBurgerClick);
 function handlerBurgerClick() {
     if (!isMenuOpen) {
         burger.classList.add('active');
+        menu.classList.add('active');
         isMenuOpen = true;
     } else {
         burger.classList.remove('active');
+        menu.classList.remove('active');
         isMenuOpen = false;
     }
 }
